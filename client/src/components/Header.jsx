7a13f4b835b00ce9b005e2c1dfe00db71a4144ee
Afiya Iram <afiyairum78@gmail.com>
2024-09-15 04:59:48 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,10 +2,15 @@ import { Button, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
+import { FaMoon, FaSun } from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggletheme } from '../redux/theme/themeSlice'
+
 
 export default function Header() {
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
+    const {theme} = useSelector((state) => state.theme)
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -25,8 +30,8 @@ export default function Header() {
             <AiOutlineSearch/>
         </Button>
        <div className='flex gap-2 md:order-2 '>
-       <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-            <FaMoon/>
+       <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() => dispatch(toggletheme())}>
+            {theme === 'light' ? <FaSun/> : <FaMoon/>}
         </Button>
         <Link to='/sign-in'>
             <button  className='px-1 py-1 bg-gradient-to-r from-pink-500 to-blue-300 text-white rounded-lg'  >
